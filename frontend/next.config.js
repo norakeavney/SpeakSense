93c7e084729f3fab73e3ffcd7b27ab6092989f28
norakeavney <g00415845@atu.ie>
2026-03-10 20:09:21 +0000

    updated docker stuff

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Allow API requests to Django backend
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*',
+        destination: `${process.env.BACKEND_URL || 'http://127.0.0.1:8000'}/api/:path*`,
       },
     ];
   },
